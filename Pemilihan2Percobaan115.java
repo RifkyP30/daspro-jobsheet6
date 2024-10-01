f247bb0a5f17fddb0f435debb45c68476b0ab58e
Rifky <rifkypraditya1025@gmail.com>
2024-10-01 17:09:55 +0700

    menambah file pemilihan 2 percobaan 1

---
+++ Pemilihan2Percobaan115.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan115 {
+  public static void main(String[] args) {
+    Scanner input15 = new Scanner(System.in);
+    int tahun;
+
+    System.out.print("Masukkan tahun: ");
+    tahun = input15.nextInt();
+
+    if (tahun % 4 == 0) {
+      if (tahun % 100 != 0) {
+        System.out.println("Tahun kabisat");
+      }
+    }
+    else {
+      System.out.println("Bukan tahun kabisat");
+    }
+  }
+}
